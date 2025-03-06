7132c2787ae7fbb0cfe4ce31e2f2cb4eeb78d5ec
TomBearpark <t.bearpark@googlemail.com>
2025-03-06 10:01:48 -0500

    initial commit

---
+++ code/0_funcs.R
@@ -0,0 +1,249 @@
+# CONTENTS:
+## 1. Running regressions:
+  # loading historic data
+  # running regression
+  # plotting marginal effects
+
+## 2. Prepping data for projection
+
+# RUNNING REGRESSIONS -----------------------------------------------------
+
+load_historic_data <- function(dir){
+  df.in <- read_rds(file.path(dir,"temp_gdp_world_panel.rds")) %>% 
+    rename(temp1 = era_mwtemp, pop=SP.POP.TOTL) %>%
+    group_by(ID=ISO3) %>% 
+    arrange(ID, year) %>% 
+    mutate(y = NY.GDP.PCAP.KD, 
+           dy = log(y) - lag(log(y)), 
+           temp2 = temp1^2, 
+           time1 = year - 1960, 
+           time2 = time1^2, 
+           Tbar = mean(temp1, na.rm=TRUE), 
+           dtemp1 = temp1-lag(temp1), 
+           dtemp2 = temp2-lag(temp2)
+    ) %>% 
+    ungroup()  %>% 
+    filter(year >= 1960 & year <= 2019)  %>% 
+    filter(!is.na(y), !is.na(temp1)) %>% 
+    group_by(ID) %>% 
+    useful::add_lags(vars = c("temp", "dtemp"), lags=10, max.p=2, 
+                     sort_df = FALSE) %>% 
+    select(ID, year, time1, time2, y, g=dy, pop, contains("temp"))
+}
+
+get_var <- function(type){
+  if(type=="levels") var <- "dtemp"
+  else if(type == "growth") var <- "temp"
+  else stop("not implemented")
+  return(var)
+}
+
+run_reg <- function(df, 
+                    type="levels", 
+                    lags=1, 
+                    spec="poly2", 
+                    FE = "ID + time1 + ID[time1]"){
+  
+  var <- get_var(type)
+  poly_order <- as.numeric(str_extract(spec, "[0-9]"))
+  if(lags==0) var <- paste0("l0_", var)
+  
+  ff <- useful::build_formula_poly(
+    yvar = "g", treat = var, poly_treat = poly_order, control=NULL, 
+    leads = 0, lags=lags , FE=FE
+  )
+  feols(
+    ff, 
+    data = df, 
+    cluster = "ID"
+  )
+}
+
+plot_me <- function(pdf, type, lags=NULL){
+  
+  p <- ggplot(pdf, 
+              aes(temp, estimate)) + 
+    geom_hline(yintercept=0, linetype="dashed") +
+    geom_line() + 
+    geom_ribbon(aes(temp, ymin = conf.low, ymax = conf.high), 
+                alpha = .2) + 
+    ggtitle(type) + xlab("Temp") + ylab("ME")
+  
+  if(!is.null(lags)) p <- p+facet_wrap(~lag) 
+  
+  p
+}
+
+get_me_sep <- function(m, lags, type="growth", xrange=seq(0, 30, by = 5), id="", 
+                   plot=T){
+  
+  var <- get_var(type)
+  
+  pdf <- map_dfr(
+    0:lags, 
+    function(lag){
+      map_dfr(
+        xrange, 
+        function(temp){
+          hypotheses(m, 
+                     paste0(  "l", lag, "_", var, "1 + ", 
+                            "2*l", lag, "_", var, "2*",temp," = 0")) %>% 
+            broom::tidy() %>% 
+            mutate(temp = !!temp, lag=!!lag)
+          
+        }
+      ) %>% 
+        mutate(id = !!id, lag=as.character(lag))
+    }
+  )
+  if(plot){
+    return(plot_me(pdf, type, lags))
+  }else{
+    return(pdf)
+  }
+}
+
+get_me_cum <- function(m, lags, type="growth", xrange=seq(0, 30, by = 5), id="", 
+                   plot=T){
+  
+  var <- get_var(type)
+  
+  pdf <-
+      map_dfr(
+        xrange, 
+        function(temp){
+          ff <- ""
+          for(lag in 0:lags){
+            ff <- paste0(ff, 
+                         paste0(  "l", lag, "_", var, "1 + ", 
+                                "2*l", lag, "_", var, "2*",temp," + "))
+          }
+          hypotheses(m, 
+                     paste0(ff, " = 0")) %>% 
+            broom::tidy() %>% 
+            mutate(temp = !!temp, lag="Cumulative")
+          
+        }
+      ) %>% 
+        mutate(id = !!id)
+  if(plot){
+    return(plot_me(pdf, type, lags))
+  }else{
+    pdf
+  }
+}
+
+get_me <- function(m, lags, type="growth", xrange=seq(0, 30, by = 5), id="", 
+                       plot=T){
+   
+  
+  pdf.sep <- get_me_sep(m, lags, type, xrange, id, plot=FALSE) 
+  pdf.cum <- get_me_cum(m, lags, type, xrange, id, plot=FALSE) %>% 
+    select(-lag)
+  
+  if(plot){
+    return(plot_me(pdf.sep, type, lags) + 
+             geom_line(data= pdf.cum, aes(temp, estimate), 
+                       color = 'blue'))
+  }else{
+    return(pdf)
+  }
+}
+
+# PREPPING DATA -----------------------------------------------------------
+
+gen_mats <- function(NN, TT, IDs, yrs){
+  mat <- matrix(NA, nrow=NN, ncol=TT)
+  rownames(mat) <- IDs
+  colnames(mat) <- yrs
+  y <- g <- temp <- pr <- mat
+  return(list(y=y, g=g, temp=temp, pr=pr))
+}
+
+check_ID_order <- function(mat, df, yr){
+  stopifnot(rownames(mat)==df %>% 
+              filter(year == yr) %>% arrange(ID) %>% 
+              pull(ID))
+}
+
+format_pre_proj <- function(pre.ids, pre.yrs, df, base, proj){
+  
+  check_ID_order(base$temp, df, pre.yrs[1])
+  
+  for(tt in pre.ids){
+    
+    yr <- pre.yrs[tt]
+    
+    # -- Temperature
+    base$temp[, tt] <- proj$temp[, tt] <- df %>% 
+      filter(year == yr) %>% arrange(ID) %>% pull(temp1)
+    
+    # --- Growth
+    base$g[, tt] <- proj$g[, tt] <- df %>% 
+      filter(year == yr) %>% arrange(ID) %>% pull(g)
+    
+    # --- GDP 
+    base$y[, tt] <- proj$y[, tt] <- df %>% 
+      filter(year == yr) %>% arrange(ID) %>% pull(y)
+  }
+  return(list(base=base, proj=proj))
+}
+
+format_post_proj_baseline <- function(post.ids, base, proj, warming, TT){
+  
+  for(tt in post.ids){
+    
+    # -- Temperature
+    base$temp[,tt] <- df.base$temp  
+    proj$temp[,tt] <- df.base$temp + warming / TT * (tt-min(post.ids))
+    
+    # -- Growth
+    base$g[,tt] <- df.base$g
+    
+    # -- GDP
+    base$y[,tt] <- (1 + base$g[, tt]) * base$y[,tt-1]
+    
+  }
+  return(list(base=base, proj=proj))
+}
+
+plot_proj <- function(pdf, vline=2020){
+  pdf %>% 
+    ggplot() + 
+    geom_vline(xintercept=vline, linetype=2)+
+    geom_line(aes(x = year, y = value, color = scen)) + 
+    facet_wrap(~ID+name, scales='free')
+}
+
+plotdf_mats <- function(toPlot, base, proj, plt=T, vline=2020){
+  pdf <- map_dfr(
+    toPlot, 
+    function(ii){
+      bind_rows(
+        tibble(
+          year = as.numeric(colnames(base$y)), 
+          ID = ii, 
+          y = base$y[ii, ], 
+          g = base$g[ii, ], 
+          temp = base$temp[ii, ], 
+          dam = 0, 
+          scen = "base"
+        ), 
+        tibble(
+          year = as.numeric(colnames(proj$y)), 
+          ID = ii, 
+          y = proj$y[ii, ], 
+          g = proj$g[ii, ], 
+          temp = proj$temp[ii, ], 
+          dam = (proj$y-base$y)[ii, ], 
+          scen = "proj"
+        )
+      )
+    }
+  ) %>% 
+    pivot_longer(cols = c(temp, y, g, dam)) %>% 
+    mutate(name = fct_relevel(name, c("temp", "g", "y", "dam")))
+  
+  if(plt) return(plot_proj(pdf, vline))
+  else return(pdf)
+}
---
+++ code/run_projection.R
@@ -0,0 +1,176 @@
+if(!require(pacman)) install.packages('pacman')
+pacman::p_load(tidyverse, fixest, haven, marginaleffects, broom, useful)
+theme_set(theme_classic())
+
+dir <- "~/Library/CloudStorage/Dropbox/gdp-temp/replication/"
+code <- "~/Documents/GitHub/gdp_project/"
+
+set.seed(123)
+source(paste0(code, "code/0_funcs.R"))
+
+
+# SPECIFY PARAMETERS ------------------------------------------------------
+type <- "growth"
+lags <- 0
+spec <- "poly2"
+warming <- 2
+
+toPlot <- c("RUS", "CHN", "SDN")
+
+# LOAD DATA ---------------------------------------------------------------
+df.reg <- load_historic_data(dir)
+
+plt.temps <- df.reg %>% 
+  filter(ID %in% toPlot) %>% 
+  summarize(temp = mean(temp1), .by=ID) %>% 
+  mutate(temp_proj = temp + warming)
+
+df.reg %>% 
+  filter(year == 2019) %>% 
+  ggplot() + 
+  geom_histogram(aes(x = temp1)) + 
+  geom_vline(data = plt.temps, aes(xintercept=temp, color=ID)) + 
+  geom_vline(data = plt.temps, aes(xintercept=temp_proj, color=ID), linetype=2)
+
+# RUN REGRESSION ----------------------------------------------------------
+
+m  <- run_reg(df.reg, type=type, lags=lags, spec=spec)
+get_me_sep(m, lags, type=type)
+get_me(m, lags, type=type)
+
+me.cum <- get_me_cum(m, lags, type=type)
+me.cum + 
+  geom_vline(data = plt.temps, aes(xintercept=temp, color=ID))+
+  geom_vline(data = plt.temps, aes(xintercept=temp_proj, color=ID), linetype=2)
+
+# GET BASELINES -----------------------------------------------------------
+pre.yrs   <- 1990:2019
+proj.yrs  <- 2020:2100
+yrs       <- c(pre.yrs, proj.yrs) 
+
+TT        <- length(yrs)
+
+pre.ids  <- 1:length(pre.yrs)
+post.ids <- (length(pre.yrs)+1):length(yrs)
+yr.ids   <- c(pre.ids, post.ids) 
+
+# Filter to period we are going to use in plotting / getting baseline
+df <- df.reg %>% 
+  filter(year >= min(pre.yrs))  %>% 
+  filter(!is.na(g), !is.na(temp1), !is.na(y)) %>% 
+  group_by(ID) %>%
+  add_tally() %>%
+  ungroup() %>%
+  filter(n == max(n))
+
+# Get some averages which are used to define counterfactual
+df.base <- df %>% 
+  summarize(g = mean(g, na.rm=T), 
+            temp = mean(temp1, na.rm=T), 
+            pop = mean(pop, na.rm=T),
+            .by = ID) %>% 
+  select(ID, temp, g, pop) %>% 
+  mutate(g = if_else(g > 0.03, 0.03, g))
+
+NN <- length(unique(df$ID))
+print(NN)
+
+# Create matrices to store data
+base <- gen_mats(NN, TT, df.base$ID, yrs)
+proj <- gen_mats(NN, TT, df.base$ID, yrs)
+
+## Format pre-projection data -----------------
+pre.proj <- format_pre_proj(pre.ids, pre.yrs, df, base, proj)
+base <- pre.proj$base
+proj <- pre.proj$proj
+
+## Format post-projection baseline ------------------
+post.proj <- format_post_proj_baseline(post.ids, base, proj, warming, TT)
+base <- post.proj$base
+proj <- post.proj$proj
+
+## Sense check the data 
+plotdf_mats(toPlot, base, proj)
+
+# PROJECTION --------------------------------------------------------------
+
+b0 <- coef(m)[sort(str_subset(names(coef(m)), "temp1"))] %>% as.matrix()
+b1 <- coef(m)[sort(str_subset(names(coef(m)), "temp2"))] %>% as.matrix()
+
+
+if(type == "levels"){
+  for(tt in 2:TT) {
+    base$pr[,tt] <- base$temp[,tt]-base$temp[,tt-1]
+    proj$pr[,tt] <- proj$temp[,tt]-proj$temp[,tt-1]  
+  }
+}else{
+  base$pr <- base$temp
+  proj$pr <- proj$temp
+}
+
+for(tt in post.ids){
+  lls <- tt-0:lags
+  
+  delta <- (proj$pr[, lls]   - base$pr[, lls]) %*% b0 + 
+           (proj$pr[, lls]^2 - base$pr[, lls]^2) %*% b1
+    
+  # -- Growth
+  proj$g[,tt] <- base$g[, tt]+delta
+  
+  # -- GDP
+  proj$y[,tt] <- (1 + proj$g[, tt]) * proj$y[,tt-1]
+    
+}
+
+plotdf_mats(toPlot, base, proj, plt=T)
+
+pdf.all <- plotdf_mats(df.base$ID, base, proj, plt=F)%>% 
+  left_join(df.base) %>% 
+  filter(name == 'y') %>% 
+  pivot_wider(names_from = scen, values_from = value) %>% 
+  mutate(pc_dam = proj - base, 
+         tot_dam = pc_dam * pop) 
+
+pdf.all %>% 
+  ggplot() + 
+  geom_line(aes(x = year, y = pc_dam, group = ID), alpha=.4)
+pdf.all %>% 
+  ggplot() + 
+  geom_line(aes(x = year, y = tot_dam, group = ID), alpha=.4)
+
+pdf.all %>% filter(year == 2100) %>% 
+  arrange(pc_dam)
+
+pdf.all %>% 
+  filter(year == 2100) %>% 
+  pivot_longer(cols = c(pc_dam, tot_dam), 
+               names_to = 'type', values_to = 'dam') %>% 
+  ggplot() + geom_point(aes(x = temp, y = dam))+
+  geom_label(aes(x = temp, y = dam, label = ID)) + 
+  facet_wrap(~type, scales='free')
+
+pdf.all  %>% filter(year == 2100) %>% 
+  mutate(rDam = rank(pc_dam), 
+         rTemp = rank(temp)) %>%
+  ggplot() + 
+  geom_point(aes(x= rTemp, y = rDam))
+
+# CALCULATE DAMAGES  ------------------------------------------------------
+dim(proj$y)
+length(df.base$pop)
+
+dam.out <- map_dfr(
+  yr.ids, 
+  function(tt){
+    pc.diff    <- proj$y[,tt]-base$y[,tt]
+    tot.damage <- sum(pc.diff * df.base$pop)
+    glob.gdp   <- sum(proj$y[,tt]*df.base$pop)
+    tibble(year = yrs[tt], 
+           damage=100*tot.damage / glob.gdp)
+  }
+)
+
+dam.out %>% 
+  ggplot() + 
+  geom_vline(xintercept=max(pre.yrs))+
+  geom_line(aes(x = year, y = damage))
